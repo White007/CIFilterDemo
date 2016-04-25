fd4da762853e73fb3e6da94eb113aa3e69d324e8
zhumingke <ifeiteng007@sina.com>
2016-04-25 11:49:27 +0800

    Initial Commit

---
+++ CIFilter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CIFilter
+//
+//  Created by 朱明科 on 16/4/25.
+//  Copyright © 2016年 zhumingke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
